1f7a5074f6f0df54fd8f8d774182f472de177e40
Daniel Kaehn <kaehnd@msoe.edu>
2021-10-02 12:57:47 -0500

    implement GPIO ISR and setup

--- src/driver/network/network.c
+++ src/driver/network/network.c
@@ -1,13 +1,51 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "stm32f446xx.h"
+
 #include "network.h"
+#include "state.h"
+#include "timeout.h"
+
+#define EXTI_15_10_NVIC 8
+
+static volatile EXTI_TypeDef * const exti = (volatile EXTI_TypeDef *) EXTI_BASE;
+static volatile GPIO_TypeDef * const gpioc = (volatile GPIO_TypeDef *) GPIOC_BASE;
 
 ERROR_CODE network_init()
 {
-    RETURN_NO_ERROR();
+	static volatile RCC_TypeDef * const rcc = (volatile RCC_TypeDef *) RCC_BASE;
+	static volatile NVIC_Type * const nvic = (volatile NVIC_Type *) NVIC_BASE;
+	static volatile SYSCFG_TypeDef * const syscfg = (volatile SYSCFG_TypeDef *) SYSCFG_BASE;
+
+	rcc->APB2ENR |= RCC_APB2ENR_SYSCFGEN; //SYSCFGEN
+	rcc->AHB1ENR |= RCC_AHB1ENR_GPIOCEN; //GPIOCEN
+
+	gpioc->MODER &= ~GPIO_MODER_MODER12; //set PC12 as input
+
+	gpioc->PUPDR &= ~GPIO_PUPDR_PUPD12;
+	gpioc->PUPDR |= 0b01<<GPIO_PUPDR_PUPD12_Pos; //set pullup resistor
+
+	syscfg->EXTICR[3]  &= ~SYSCFG_EXTICR4_EXTI12;
+	syscfg->EXTICR[3]  |= SYSCFG_EXTICR4_EXTI12 & SYSCFG_EXTICR4_EXTI12_PC;  //EXTI line 12 set to bank C
+
+	nvic->ISER[1] |= 1<<EXTI_15_10_NVIC; //Set enable in NVIC, NVIC POS 40
+
+	exti->IMR |= EXTI_IMR_IM12; //Unmask in EXTI
+	exti->FTSR |= EXTI_FTSR_TR12; //Set falling trigger
+	exti->RTSR |= EXTI_RTSR_TR12; //set rising trigger
+
+	RETURN_NO_ERROR();
 }
 
-//ISR
-//or some other interrupt
-void EXTI0_IRQHandler()
+void EXTI15_10_IRQHandler()
 {
-
+	__asm__("CPSID i"); //disable interrupts
+	if (exti->PR & EXTI_PR_PR12) 
+	{
+		bool on = gpioc->IDR >> GPIO_IDR_ID12_Pos;
+		exti->PR = EXTI_PR_PR12; //clear pending interrupt
+		state_set(BUSY);
+    timeout_reset();
+	}
+	__asm__("CPSIE i"); //enable interrupts
 }
