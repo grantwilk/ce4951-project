9c383f47677b6faf5fc12430f0f649606fdde8ce
schlavenskyl <schlavenskyl@msoe.edu>
2021-10-03 14:51:46 -0500

    Luke Schlavensky 10/3/21 - Finished most of the functionality for state.c and state.h - Throwing an error is not set yet because I did not want to add an error to errors.h and overwrite someone else's error

--- src/state.c
+++ src/state.c
@@ -1,12 +1,56 @@
-#include "state.h"
+/*
+ * Luke Schlavensky
+ * 10/3/21
+ * state.c
+ *
+ * Description: This file is responsible
+ * for setting and getting the current state
+ */
 
-ERROR_CODE state_set(state_t state)
+#include "state.h"
+//#include "error.h"
+
+
+//Current State
+STATE_TYPE current_state = IDLE;
+
+
+/**
+ * @brief   This function sets the current state
+ *
+ * @param   STATE_TYPE state - Try to switch to that state
+ *
+ * @return  ERROR_CODE - Throws no error unless the param is not a state
+ */
+ERROR_CODE state_set(STATE_TYPE state)
 {
+    if(state == IDLE)
+    {
+        current_state = IDLE;
+    }else if(state == BUSY)
+    {
+        current_state = BUSY;
+    }else if(state == COLLISION)
+    {
+        current_state = COLLISION;
+    }else
+    {
+        //Throw an Error here
+    }
+
     RETURN_NO_ERROR();
 }
 
-state_t state_get()
+
+/**
+ * @brief   This function returns the current state
+ *
+ * @param   void
+ *
+ * @return  STATE_TYPE - The current state
+ */
+STATE_TYPE state_get()
 {
-    return 0;
+    return current_state;
 }
 
