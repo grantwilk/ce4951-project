bfa57e89dc3fedf7ba6c865436da642d26b51bbf
Grant Wilk <grant@remington.pro>
2021-10-06 14:34:16 -0500

    Created branch for @rectorj's changes

--- src/driver/leds/leds.c
+++ src/driver/leds/leds.c
@@ -2,15 +2,68 @@
 
 ERROR_CODE led_init()
 {
+    // SET UP PC8(red), PC6(yellow), PC5(green) in output pull up resistor mode
+    rcc->AHB2RSTR |= RCC_APB2ENR_SYSCFGEN; // System config enabled
+    rcc->AHB1ENR |= RCC_AHB1ENR_GPIOCEN; // GPIO C enabled
+
+    gpioc->MODER |= GPIO_MODER_MODER8; // GPIO c8 Output
+    gpioc->MODER |= GPIO_MODER_MODER6; // GPIO c6 Output
+    gpioc->MODER |= GPIO_MODER_MODER5; // GPIO c5 Output
+
+    gpioc->PUPDR &= ~GPIO_PUPDR_PUPD8; // GPIO c8 pull up
+    gpioc->PUPDR |= 0b01 << GPIO_PUPDR_PUPD8_Pos;
+    gpioc->PUPDR &= ~GPIO_PUPDR_PUPD6; // GPIO c6 pull up
+    gpioc->PUPDR |= 0b01 << GPIO_PUPDR_PUPD6_Pos;
+    gpioc->PUPDR &= ~GPIO_PUPDR_PUPD5; // GPIO c5 pull up
+    gpioc->PUPDR |= 0b01 << GPIO_PUPDR_PUPD5_Pos;
+
+    initialized = true;
+    leds_clear(); // set all to off at the start
+
     RETURN_NO_ERROR();
 }
 
-ERROR_CODE led_clear()
+ERROR_CODE leds_clear()
 {
+    if(initialized) {
+        //pull up resistor leads to logic 1 being off
+        gpioc->ODR |= (GPIO_ODR_OD8 | GPIO_ODR_OD6 | GPIO_ODR_OD5); // set all to off
+    } else {
+        //if not initialized ERROR
+        THROW_ERROR(0x07);
+    }
     RETURN_NO_ERROR();
 }
 
 ERROR_CODE led_set(leds_t led, bool set)
 {
+   if(initialized) {
+       if(set) {
+           switch(led) {
+               //pull up resistor leads to logic 0 being ON
+               case LED_RED : gpioc->ODR &= ~GPIO_ODR_OD8;
+                   break;
+               case LED_YELLOW : gpioc->ODR &= ~GPIO_ODR_OD8;
+                   break;
+               case LED_GREEN : gpioc->ODR &= ~GPIO_ODR_OD8;
+                   break;
+               default : THROW_ERROR(0x01);
+           }
+       }else {
+           switch(led) {
+               //pull up resistor leads to logic 1 being off
+               case LED_RED : gpioc->ODR |= GPIO_ODR_OD8;
+                   break;
+               case LED_YELLOW : gpioc->ODR |= GPIO_ODR_OD8;
+                   break;
+               case LED_GREEN : gpioc->ODR |= GPIO_ODR_OD8;
+                   break;
+               default : THROW_ERROR(0x01);
+           }
+       }
+   } else {
+       //if not initialized ERROR
+       THROW_ERROR(0x07);
+   }
     RETURN_NO_ERROR();
 }
--- src/driver/leds/leds.h
+++ src/driver/leds/leds.h
@@ -1,13 +1,18 @@
 #ifndef DRIVER_LEDS_H
 #define DRIVER_LEDS_H
 
+#include <stdint.h>
 #include <stdbool.h>
+#include "stm32f446xx.h"
 #include "error.h"
 
 typedef enum leds { LED_GREEN, LED_RED, LED_YELLOW } leds_t;
+static volatile GPIO_TypeDef *const gpioc = (volatile GPIO_TypeDef *)GPIOC_BASE;
+static volatile RCC_TypeDef *const rcc = (volatile RCC_TypeDef *)RCC_BASE;
+bool initialized = false;
 
 ERROR_CODE leds_init();
 ERROR_CODE leds_clear();
 ERROR_CODE leds_set(leds_t led, bool set);
 
-#endif // DRIVER_LEDS_H
+#endif// DRIVER_LEDS_H
